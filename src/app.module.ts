e33d441fad561e3e276ddc0f4b47a207bc5001db
trickzhavia <patrick@zhavia.com.br>
2023-07-27 09:42:01 -0300

    feat: root modules

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [UsersModule, PostsModule, DatabaseModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
